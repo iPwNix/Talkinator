9a346598bdb3121b61c03d76f2f40c63259cb9f1
Erik V <erik_vlasblom@hotmail.com>
2019-04-17 04:03:50 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users/{username}', 'ProfileController@show')->name('showProfile');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users/{username}/settings', 'ProfileController@editingInfo')->name('editInfo');
+    Route::post('/users/{username}/settings', 'ProfileController@updateInfo')->name('updateInfo');
+    Route::get('/users/{username}/settings/avatar', 'ProfileController@editingAvatar')->name('editAvatar');
+    Route::post('/users/{username}/settings/avatar', 'ProfileController@updateAvatar')->name('updateAvatar');
+    Route::get('/users/{username}/settings/cover', 'ProfileController@editingCover')->name('editCover');
+    Route::post('/users/{username}/settings/cover', 'ProfileController@updateCover')->name('updateCover');
+});
+
 Auth::routes();
 
 Route::get('logout', 'Auth\LoginController@logout', function () {
